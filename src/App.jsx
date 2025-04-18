a5ffc8eb3d275034b316e26a7b3d62312173c3eb
Arthur <arthurfangmaier@gmail.com>
2025-04-18 19:16:28 +0300

    refactor: adjust project structure for better organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-
+import { PrimeReactProvider } from "primereact/api";
 import Header from "./components/Header";
+import Promo from "./components/PromoSection";
+import About from "./components/AboutSection";
 import Footer from "./components/Footer";
 import Button from "./components/Button";
 import Title from "./components/Title";
@@ -8,24 +10,106 @@ import Services from "./components/ServicesBlock";
 import Advantages from "./components/AdvantagesBlock";
 import Form from "./components/Form";
 
-class App extends React.Component{
-  render(){
-    return(
+class App extends React.Component {
+  render() {
+    return (
       <>
-        text check
-        <div className="hilights">hilights</div>
+          <Header />
+          <Promo />
+          <About />
       </>
-    )
+    );
   }
 }
 
 export default App;
 
-
-{/* <Header />
+{
+  /* 
 <Footer />
 <Button />
 <Title />
 <Services />
 <Advantages />
-<Form /> */}
+<Form /> */
+}
+
+// 1 вариант: index.html (точка входа) -> index.js -> App.js (содержит контент)
+// 2 вариант: index.html (точка входа) -> App.js -> Main.js (содержит контент)
+
+/*
+  папка config -> файл langs.js -> в ней все переводы 
+  const ENGLISH: {
+    "title": "Hello",
+    "text": "test text"
+  }
+  const FINISH:{
+    "title": "Terve",
+    "text": "testi texti"
+  }
+  const RUSSIAN: {
+    "title": "Привет",
+    "text": "тестовый текс"
+  }
+
+  export const getLang = (code) =>{
+    if (code == "EN") return ENGLISH
+    if (code == "FI") return FINISH
+    if (code == "RU") return RUSSIAN
+  }
+
+-------------------------------------------------------
+
+  в компоненте App.js создаем переменную, которая хранит текущий код языка (не код программерский!!!) для сайта и переменную, которая содержит текущие переводы (ключи из объекта)
+
+  import LangMenu from "/path"
+  import getLang from ./config/langConfig
+
+  const [langCode, setLangCode] = useState("EN") // хранит текущий код языка (не код программерский!!!) 
+  const [lang, setLang] = useState(getLang("EN")) //содержит текущие переводы (ключи из объекта)
+
+  useEffect(()=>{
+    setLang(getLang(langCode))
+  }, [langCode])
+
+  render(){
+    return(
+      <>
+        <h1>{lang.title}</h1>
+        <LangMenu setLangCode={setLangCode} lang={lang}/> // отдаем setLangCode как параметр, чтобы LangMenu мог использовтаь
+        <Header lang={lang} />
+      </>
+    )
+  }
+
+--------------------------------------------------------
+компонент языковое меню
+
+export cont LangMenu = ({setLangCode, lang}) => {
+
+const handleLang = (newLang) => {
+  setLangCode(newLang)
+}
+
+<ul>
+  <li onClick={handleLang("EN")}>English</li>
+  <li onClick={handleLang("FI")}>Finnish</li>
+  <li onClick={handleLang("RU")}>Russian</li>
+</ul>
+
+}
+---------------
+export const Header = ({lang}) => {
+
+    render(){
+    return(
+      <>
+          <ul>
+            <li>{lang.textAboutMe}</li>
+            <li>{lang.prices}</li>
+          </ul>
+      </>
+    )
+  }
+}
+*/
--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,6 @@ import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 import './assets/css/main.css'
 
-const app = ReactDOMClient.createRoot(document.getElementById("app")); 
+const app = ReactDOMClient.createRoot(document.getElementById("app"));
+
 app.render(<App />);
